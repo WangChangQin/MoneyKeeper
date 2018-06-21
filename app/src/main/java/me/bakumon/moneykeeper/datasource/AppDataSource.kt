e760eeffeeea263890c37e444cf03b2738f473cc
Bakumon <bakumon.me@gmail.com>
2018-06-21 10:43:27 +0800

    💰 landing kotlin

--- app/src/main/java/me/bakumon/moneykeeper/datasource/AppDataSource.kt
+++ app/src/main/java/me/bakumon/moneykeeper/datasource/AppDataSource.kt
@@ -14,121 +14,127 @@
  *  limitations under the License.
  */
 
-package me.bakumon.moneykeeper.datasource;
+package me.bakumon.moneykeeper.datasource
 
-import java.util.Date;
-import java.util.List;
+import java.util.Date
 
-import io.reactivex.Completable;
-import io.reactivex.Flowable;
-import me.bakumon.moneykeeper.database.entity.DaySumMoneyBean;
-import me.bakumon.moneykeeper.database.entity.Record;
-import me.bakumon.moneykeeper.database.entity.RecordType;
-import me.bakumon.moneykeeper.database.entity.RecordWithType;
-import me.bakumon.moneykeeper.database.entity.SumMoneyBean;
-import me.bakumon.moneykeeper.database.entity.TypeSumMoneyBean;
-import me.bakumon.moneykeeper.ui.addtype.TypeImgBean;
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import me.bakumon.moneykeeper.database.entity.DaySumMoneyBean
+import me.bakumon.moneykeeper.database.entity.Record
+import me.bakumon.moneykeeper.database.entity.RecordType
+import me.bakumon.moneykeeper.database.entity.RecordWithType
+import me.bakumon.moneykeeper.database.entity.SumMoneyBean
+import me.bakumon.moneykeeper.database.entity.TypeSumMoneyBean
+import me.bakumon.moneykeeper.ui.addtype.TypeImgBean
 
 /**
  * 数据源
  *
  * @author Bakumon https://bakumon.me
  */
-public interface AppDataSource {
+interface AppDataSource {
     /**
      * 获取所有记账类型数据
      *
      * @return 所有记账类型数据
      */
-    Flowable<List<RecordType>> getAllRecordType();
-
-    /**
-     * 初始化默认的记账类型
-     */
-    Completable initRecordTypes();
+    val allRecordType: Flowable<List<RecordType>>
 
     /**
      * 获取当前月份的记账记录数据
      *
      * @return 当前月份的记录数据的 Flowable 对象
      */
-    Flowable<List<RecordWithType>> getCurrentMonthRecordWithTypes();
+    val currentMonthRecordWithTypes: Flowable<List<RecordWithType>>
+
+    /**
+     * 获取本月支出和收入总数
+     */
+    val currentMonthSumMoney: Flowable<List<SumMoneyBean>>
+
+    /**
+     * 初始化默认的记账类型
+     */
+    fun initRecordTypes(): Completable
 
     /**
      * 根据类型获取某段时间的记账记录数据
      *
      * @return 包含记录数据的 Flowable 对象
      */
-    Flowable<List<RecordWithType>> getRecordWithTypes(Date dateFrom, Date dateTo, int type);
+    fun getRecordWithTypes(dateFrom: Date, dateTo: Date, type: Int): Flowable<List<RecordWithType>>
 
     /**
      * 获取某一类型某段时间的记账记录数据
      *
      * @return 包含记录数据的 Flowable 对象
      */
-    Flowable<List<RecordWithType>> getRecordWithTypes(Date dateFrom, Date dateTo, int type, int typeId);
+    fun getRecordWithTypes(dateFrom: Date, dateTo: Date, type: Int, typeId: Int): Flowable<List<RecordWithType>>
 
     /**
      * 获取某一类型某段时间的记账记录数据，money 排序
      *
      * @return 包含记录数据的 Flowable 对象
      */
-    Flowable<List<RecordWithType>> getRecordWithTypesSortMoney(Date dateFrom, Date dateTo, int type, int typeId);
+    fun getRecordWithTypesSortMoney(dateFrom: Date, dateTo: Date, type: Int, typeId: Int): Flowable<List<RecordWithType>>
 
     /**
      * 新增一条记账记录
      *
      * @param record 记账记录实体
      */
-    Completable insertRecord(Record record);
+    fun insertRecord(record: Record): Completable
 
     /**
      * 更新一条记账记录
      *
      * @param record 记录对象
      */
-    Completable updateRecord(Record record);
+    fun updateRecord(record: Record): Completable
 
     /**
      * 删除一天记账记录
      *
      * @param record 要删除的记账记录
      */
-    Completable deleteRecord(Record record);
+    fun deleteRecord(record: Record): Completable
 
     /**
      * 记账类型排序
      *
      * @param recordTypes 记账类型对象
      */
-    Completable sortRecordTypes(List<RecordType> recordTypes);
+    fun sortRecordTypes(recordTypes: List<RecordType>): Completable
 
     /**
      * 删除记账类型
      *
      * @param recordType 要删除的记账类型对象
      */
-    Completable deleteRecordType(RecordType recordType);
+    fun deleteRecordType(recordType: RecordType): Completable
 
     /**
      * 获取指出或收入记账类型数据
      *
      * @param type 类型
      * @return 记账类型数据
-     * @see RecordType#TYPE_OUTLAY
-     * @see RecordType#TYPE_INCOME
+     * @see RecordType.TYPE_OUTLAY
+     *
+     * @see RecordType.TYPE_INCOME
      */
-    Flowable<List<RecordType>> getRecordTypes(int type);
+    fun getRecordTypes(type: Int): Flowable<List<RecordType>>
 
     /**
      * 获取类型图片数据
      *
      * @param type 收入或支出类型
      * @return 所有获取类型图片数据
-     * @see RecordType#TYPE_OUTLAY
-     * @see RecordType#TYPE_INCOME
+     * @see RecordType.TYPE_OUTLAY
+     *
+     * @see RecordType.TYPE_INCOME
      */
-    Flowable<List<TypeImgBean>> getAllTypeImgBeans(int type);
+    fun getAllTypeImgBeans(type: Int): Flowable<List<TypeImgBean>>
 
     /**
      * 添加一个记账类型
@@ -136,10 +142,11 @@ public interface AppDataSource {
      * @param type    类型
      * @param imgName 图片
      * @param name    类型名称
-     * @see RecordType#TYPE_OUTLAY
-     * @see RecordType#TYPE_INCOME
+     * @see RecordType.TYPE_OUTLAY
+     *
+     * @see RecordType.TYPE_INCOME
      */
-    Completable addRecordType(int type, String imgName, String name);
+    fun addRecordType(type: Int, imgName: String, name: String): Completable
 
     /**
      * 修改记账类型
@@ -147,17 +154,12 @@ public interface AppDataSource {
      * @param oldRecordType 修改之前的 RecordType
      * @param recordType    修改的 RecordType
      */
-    Completable updateRecordType(RecordType oldRecordType, RecordType recordType);
+    fun updateRecordType(oldRecordType: RecordType, recordType: RecordType): Completable
 
     /**
      * 获取本月支出和收入总数
      */
-    Flowable<List<SumMoneyBean>> getCurrentMonthSumMoney();
-
-    /**
-     * 获取本月支出和收入总数
-     */
-    Flowable<List<SumMoneyBean>> getMonthSumMoney(Date dateFrom, Date dateTo);
+    fun getMonthSumMoney(dateFrom: Date, dateTo: Date): Flowable<List<SumMoneyBean>>
 
     /**
      * 获取某天的合计
@@ -166,10 +168,10 @@ public interface AppDataSource {
      * @param month 月
      * @param type  类型
      */
-    Flowable<List<DaySumMoneyBean>> getDaySumMoney(int year, int month, int type);
+    fun getDaySumMoney(year: Int, month: Int, type: Int): Flowable<List<DaySumMoneyBean>>
 
     /**
      * 获取按类型汇总数据
      */
-    Flowable<List<TypeSumMoneyBean>> getTypeSumMoney(Date from, Date to, int type);
+    fun getTypeSumMoney(from: Date, to: Date, type: Int): Flowable<List<TypeSumMoneyBean>>
 }
---
+++ app/src/main/java/me/bakumon/moneykeeper/datasource/LocalAppDataSource.kt
@@ -0,0 +1,257 @@
+/*
+ * Copyright 2018 Bakumon. https://github.com/Bakumon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package me.bakumon.moneykeeper.datasource
+
+import android.text.TextUtils
+
+import java.util.ArrayList
+import java.util.Date
+
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import me.bakumon.moneykeeper.App
+import me.bakumon.moneykeeper.ConfigManager
+import me.bakumon.moneykeeper.R
+import me.bakumon.moneykeeper.database.AppDatabase
+import me.bakumon.moneykeeper.database.entity.DaySumMoneyBean
+import me.bakumon.moneykeeper.database.entity.Record
+import me.bakumon.moneykeeper.database.entity.RecordType
+import me.bakumon.moneykeeper.database.entity.RecordWithType
+import me.bakumon.moneykeeper.database.entity.SumMoneyBean
+import me.bakumon.moneykeeper.database.entity.TypeSumMoneyBean
+import me.bakumon.moneykeeper.ui.addtype.TypeImgBean
+import me.bakumon.moneykeeper.utill.BackupUtil
+import me.bakumon.moneykeeper.utill.DateUtils
+
+/**
+ * 数据源本地实现类
+ *
+ * @author Bakumon https://bakumon.me
+ */
+class LocalAppDataSource(private val mAppDatabase: AppDatabase) : AppDataSource {
+
+    override val allRecordType: Flowable<List<RecordType>>
+        get() = mAppDatabase.recordTypeDao().allRecordTypes
+
+    override val currentMonthRecordWithTypes: Flowable<List<RecordWithType>>
+        get() {
+            val dateFrom = DateUtils.getCurrentMonthStart()
+            val dateTo = DateUtils.getCurrentMonthEnd()
+            return mAppDatabase.recordDao().getRangeRecordWithTypes(dateFrom, dateTo)
+        }
+
+    override val currentMonthSumMoney: Flowable<List<SumMoneyBean>>
+        get() {
+            val dateFrom = DateUtils.getCurrentMonthStart()
+            val dateTo = DateUtils.getCurrentMonthEnd()
+            return mAppDatabase.recordDao().getSumMoney(dateFrom, dateTo)
+        }
+
+    /**
+     * 自动备份
+     */
+    @Throws(Exception::class)
+    private fun autoBackup() {
+        if (ConfigManager.isAutoBackup()) {
+            val isSuccess = BackupUtil.autoBackup()
+            if (!isSuccess) {
+                throw BackupFailException()
+            }
+        }
+    }
+
+    /**
+     * 自动备份
+     */
+    @Throws(Exception::class)
+    private fun autoBackupForNecessary() {
+        if (ConfigManager.isAutoBackup()) {
+            val isSuccess = BackupUtil.autoBackupForNecessary()
+            if (!isSuccess) {
+                throw BackupFailException()
+            }
+        }
+    }
+
+    override fun initRecordTypes(): Completable {
+        return Completable.fromAction {
+            if (mAppDatabase.recordTypeDao().recordTypeCount < 1) {
+                // 没有记账类型数据记录，插入默认的数据类型
+                mAppDatabase.recordTypeDao().insertRecordTypes(*RecordTypeInitCreator.createRecordTypeData())
+                autoBackupForNecessary()
+            }
+        }
+    }
+
+    override fun deleteRecord(record: Record): Completable {
+        return Completable.fromAction {
+            mAppDatabase.recordDao().deleteRecord(record)
+            autoBackup()
+        }
+    }
+
+    override fun getRecordWithTypes(dateFrom: Date, dateTo: Date, type: Int): Flowable<List<RecordWithType>> {
+        return mAppDatabase.recordDao().getRangeRecordWithTypes(dateFrom, dateTo, type)
+    }
+
+    override fun getRecordWithTypes(dateFrom: Date, dateTo: Date, type: Int, typeId: Int): Flowable<List<RecordWithType>> {
+        return mAppDatabase.recordDao().getRangeRecordWithTypes(dateFrom, dateTo, type, typeId)
+    }
+
+    override fun getRecordWithTypesSortMoney(dateFrom: Date, dateTo: Date, type: Int, typeId: Int): Flowable<List<RecordWithType>> {
+        return mAppDatabase.recordDao().getRecordWithTypesSortMoney(dateFrom, dateTo, type, typeId)
+    }
+
+    override fun insertRecord(record: Record): Completable {
+        return Completable.fromAction {
+            mAppDatabase.recordDao().insertRecord(record)
+            autoBackup()
+        }
+    }
+
+    override fun updateRecord(record: Record): Completable {
+        return Completable.fromAction {
+            mAppDatabase.recordDao().updateRecords(record)
+            autoBackup()
+        }
+    }
+
+    override fun sortRecordTypes(recordTypes: List<RecordType>): Completable {
+        return Completable.fromAction {
+            if (recordTypes.size > 1) {
+                val sortTypes = ArrayList<RecordType>()
+                for (i in recordTypes.indices) {
+                    val type = recordTypes[i]
+                    if (type.ranking != i.toLong()) {
+                        type.ranking = i.toLong()
+                        sortTypes.add(type)
+                    }
+                }
+                mAppDatabase.recordTypeDao().updateRecordTypes(*sortTypes.toTypedArray())
+                autoBackup()
+            }
+        }
+    }
+
+    override fun deleteRecordType(recordType: RecordType): Completable {
+        return Completable.fromAction {
+            if (mAppDatabase.recordDao().getRecordCountWithTypeId(recordType.id) > 0) {
+                recordType.state = RecordType.STATE_DELETED
+                mAppDatabase.recordTypeDao().updateRecordTypes(recordType)
+            } else {
+                mAppDatabase.recordTypeDao().deleteRecordType(recordType)
+            }
+            autoBackup()
+        }
+    }
+
+    override fun getRecordTypes(type: Int): Flowable<List<RecordType>> {
+        return mAppDatabase.recordTypeDao().getRecordTypes(type)
+    }
+
+    override fun getAllTypeImgBeans(type: Int): Flowable<List<TypeImgBean>> {
+        return Flowable.create({ e ->
+            val beans = TypeImgListCreator.createTypeImgBeanData(type)
+            e.onNext(beans)
+            e.onComplete()
+        }, BackpressureStrategy.BUFFER)
+    }
+
+    override fun addRecordType(type: Int, imgName: String, name: String): Completable {
+        return Completable.fromAction {
+            val recordType = mAppDatabase.recordTypeDao().getTypeByName(type, name)
+            if (recordType != null) {
+                // name 类型存在
+                if (recordType.state == RecordType.STATE_DELETED) {
+                    // 已删除状态
+                    recordType.state = RecordType.STATE_NORMAL
+                    recordType.ranking = System.currentTimeMillis()
+                    recordType.imgName = imgName
+                    mAppDatabase.recordTypeDao().updateRecordTypes(recordType)
+                } else {
+                    // 提示用户该类型已经存在
+                    throw IllegalStateException(name + App.getINSTANCE().getString(R.string.toast_type_is_exist))
+                }
+            } else {
+                // 不存在，直接新增
+                val insertType = RecordType(name, imgName, type, System.currentTimeMillis())
+                mAppDatabase.recordTypeDao().insertRecordTypes(insertType)
+            }
+            autoBackup()
+        }
+    }
+
+    override fun updateRecordType(oldRecordType: RecordType, recordType: RecordType): Completable {
+        return Completable.fromAction {
+            val oldName = oldRecordType.name
+            val oldImgName = oldRecordType.imgName
+            if (!TextUtils.equals(oldName, recordType.name)) {
+                val recordTypeFromDb = mAppDatabase.recordTypeDao().getTypeByName(recordType.type, recordType.name)
+                if (recordTypeFromDb != null) {
+                    if (recordTypeFromDb.state == RecordType.STATE_DELETED) {
+
+                        // 1。recordTypeFromDb 改成正常状态，name改成recordType#name，imageName同理
+                        // 2。更新 recordTypeFromDb
+                        // 3。判断是否有 oldRecordType 类型的 record 记录
+                        // 4。如果有记录，把这些记录的 type_id 改成 recordTypeFromDb.id
+                        // 5。删除 oldRecordType 记录
+
+                        recordTypeFromDb.state = RecordType.STATE_NORMAL
+                        recordTypeFromDb.name = recordType.name
+                        recordTypeFromDb.imgName = recordType.imgName
+                        recordTypeFromDb.ranking = System.currentTimeMillis()
+
+                        mAppDatabase.recordTypeDao().updateRecordTypes(recordTypeFromDb)
+
+                        val recordsWithOldType = mAppDatabase.recordDao().getRecordsWithTypeId(oldRecordType.id)
+                        if (recordsWithOldType != null && recordsWithOldType.size > 0) {
+                            for (record in recordsWithOldType) {
+                                record.recordTypeId = recordTypeFromDb.id
+                            }
+                            mAppDatabase.recordDao().updateRecords(*recordsWithOldType.toTypedArray())
+                        }
+
+                        mAppDatabase.recordTypeDao().deleteRecordType(oldRecordType)
+                    } else {
+                        // 提示用户该类型已经存在
+                        throw IllegalStateException(recordType.name + App.getINSTANCE().getString(R.string.toast_type_is_exist))
+                    }
+                } else {
+                    mAppDatabase.recordTypeDao().updateRecordTypes(recordType)
+                }
+            } else if (!TextUtils.equals(oldImgName, recordType.imgName)) {
+                mAppDatabase.recordTypeDao().updateRecordTypes(recordType)
+            }
+            autoBackup()
+        }
+    }
+
+    override fun getMonthSumMoney(dateFrom: Date, dateTo: Date): Flowable<List<SumMoneyBean>> {
+        return mAppDatabase.recordDao().getSumMoney(dateFrom, dateTo)
+    }
+
+    override fun getDaySumMoney(year: Int, month: Int, type: Int): Flowable<List<DaySumMoneyBean>> {
+        val dateFrom = DateUtils.getMonthStart(year, month)
+        val dateTo = DateUtils.getMonthEnd(year, month)
+        return mAppDatabase.recordDao().getDaySumMoney(dateFrom, dateTo, type)
+    }
+
+    override fun getTypeSumMoney(from: Date, to: Date, type: Int): Flowable<List<TypeSumMoneyBean>> {
+        return mAppDatabase.recordDao().getTypeSumMoney(from, to, type)
+    }
+}
